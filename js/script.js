b70616918fbb2a898a2fdc915ca490d383edfa03
mh neloy <mhneloy708@gmail.com>
2024-09-23 20:51:32 +0600

    first page js function work is complete

---
+++ js/script.js
@@ -0,0 +1,76 @@
+const header = document.getElementsByTagName("header")[0];
+window.addEventListener("scroll", (e) => {
+  let value = window.scrollY;
+  if (value < 74) {
+    header.classList.add("bg-white");
+  } else {
+    header.classList.add("bg-[rgba(255,255,255,0.5)]");
+  }
+});
+
+const donationBtn = document.getElementById("donationBtn");
+const historyBtn = document.getElementById("historyBtn");
+const donationDiv = document.getElementById("donationDiv");
+const historyDiv = document.getElementById("historyDiv");
+function historyTogol(e) {
+  if (e.innerText == "Donation") {
+    donationBtn.classList.add("bg-primary", "hover:bg-primary");
+    historyBtn.classList.remove("bg-primary", "hover:bg-primary");
+    donationDiv.classList.remove("hidden");
+    historyDiv.classList.add("hidden");
+  } else {
+    historyBtn.classList.add("bg-primary", "hover:bg-primary");
+    donationBtn.classList.remove("bg-primary", "hover:bg-primary");
+    historyDiv.classList.remove("hidden");
+    donationDiv.classList.add("hidden");
+  }
+}
+
+const input = document.querySelectorAll("input[type=text]");
+const headeramount = document.getElementById("headeramount");
+const donationamount = document.querySelectorAll("#donationamount");
+const donateMoneyBtn = document.querySelectorAll("#donateMoneyBtn");
+const modal = document.getElementById("my_modal_5");
+for (let i = 0; i < donateMoneyBtn.length; i++) {
+  donateMoneyBtn[i].addEventListener("click", (e) => {
+    if (e.target.classList[0].toLowerCase() === "feni") {
+      let value = input[1].value;
+      if (value > 0) {
+        donationamount[1].innerText =
+              parseFloat(donationamount[1].innerText) + parseFloat(value);
+          headeramount.innerText = parseFloat(headeramount.innerText) - parseFloat(value);
+        input[1].value = "";
+        modal.showModal();
+      } else {
+        alert("Your amount is not valid");
+        input[1].value = "";
+      }
+    } else if (e.target.classList[0].toLowerCase() === "noakhali") {
+        let value = input[0].value;
+        if (value > 0) {
+          donationamount[0].innerText =
+            parseFloat(donationamount[0].innerText) + parseFloat(value);
+          headeramount.innerText =
+            parseFloat(headeramount.innerText) - parseFloat(value);
+          input[0].value = "";
+          modal.showModal();
+        } else {
+          alert("Your amount is not valid");
+          input[0].value = "";
+        }
+    } else {
+        let value = input[2].value;
+        if (value > 0) {
+          donationamount[2].innerText =
+            parseFloat(donationamount[2].innerText) + parseFloat(value);
+          headeramount.innerText =
+            parseFloat(headeramount.innerText) - parseFloat(value);
+          input[2].value = "";
+          modal.showModal();
+        } else {
+          alert("Your amount is not valid");
+          input[2].value = "";
+        }
+    }
+  });
+}
